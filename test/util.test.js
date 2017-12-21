dd25fd99a32a983ab0319fb7cac22e18fdf0a510
Larry Hamel <larhamel+github@gmail.com>
2017-12-20 21:22:44 -0800

    Update qunit URLs and syntax to v2

--- test/util.test.js
+++ test/util.test.js
@@ -1,45 +1,46 @@
 $(document).ready(function(){
-  module("Test utility functions");
+  QUnit.module("Test utility functions");
 
-  test("isEqual", function() {
-    deepEqual(rsolomon.util.isEqual(1, "1"), false);
-    deepEqual(rsolomon.util.isEqual(55, parseInt("55aa", 10)), true);
-    deepEqual(rsolomon.util.isEqual(undefined, null), false);
-    deepEqual(rsolomon.util.isEqual(0, ''), false);
+  QUnit.test("isEqual", function(assert) {
+    assert.deepEqual(rsolomon.util.isEqual(1, "1"), false);
+    assert.deepEqual(rsolomon.util.isEqual(55, parseInt("55aa", 10)), true);
+    assert.deepEqual(rsolomon.util.isEqual(undefined, null), false);
+    assert.deepEqual(rsolomon.util.isEqual(0, ''), false);
   });
 
-  test("reverse", function() {
-    deepEqual(rsolomon.util.reverse([1, 2, 3, 4]).length, 4);
-    deepEqual(rsolomon.util.reverse([]), []);
-    deepEqual(rsolomon.util.reverse([1, 2, 3]), [3, 2, 1]);
+  QUnit.test("reverse", function(assert) {
+    assert.deepEqual(rsolomon.util.reverse([1, 2, 3, 4]).length, 4);
+    assert.deepEqual(rsolomon.util.reverse([]), []);
+    assert.deepEqual(rsolomon.util.reverse([1, 2, 3]), [3, 2, 1]);
   });
 
-  test("flatten", function() {
-    deepEqual(rsolomon.util.flatten([]), []);
-    deepEqual(rsolomon.util.flatten([1, 2, 3]), [1, 2, 3]);
-    deepEqual(rsolomon.util.flatten([[], 1, 2, 3]), [1, 2, 3]);
-    deepEqual(rsolomon.util.flatten([1, [2], 3]), [1, 2, 3]);
-    deepEqual(rsolomon.util.flatten([1, [2, [4, 3, [5]]]]), [1, 2, 4, 3, 5]);
-    deepEqual(rsolomon.util.flatten([1, [2, 4], 3]), [1, 2, 4, 3]);
+  QUnit.test("flatten", function(assert) {
+    assert.deepEqual(rsolomon.util.flatten([]), []);
+    assert.deepEqual(rsolomon.util.flatten([1, 2, 3]), [1, 2, 3]);
+    assert.deepEqual(rsolomon.util.flatten([[], 1, 2, 3]), [1, 2, 3]);
+    assert.deepEqual(rsolomon.util.flatten([1, [2], 3]), [1, 2, 3]);
+    assert.deepEqual(rsolomon.util.flatten([1, [2, [4, 3, [5]]]]), [1, 2, 4, 3, 5]);
+    assert.deepEqual(rsolomon.util.flatten([1, [2, 4], 3]), [1, 2, 4, 3]);
   });
 
-  test("map", function() {
+  QUnit.test("map", function(assert) {
     var test1 = rsolomon.util.map([1, 2, 3], function(arg) {
       return 2 + arg;
     });
-    deepEqual(test1, [3, 4, 5]);
+    assert.deepEqual(test1, [3, 4, 5]);
 
     var test2 = rsolomon.util.map([5, 6, 7], function(arg) {
       return arg;
     });
-    deepEqual(test2, [5, 6, 7]);
+    assert.deepEqual(test2, [5, 6, 7]);
 
     var test3 = rsolomon.util.map([5, "5", 5], function(arg) {
       return function(x) {
         return x + 5;
       }(arg);
     });
-    deepEqual(test3, [10, 55, 10]);
+    assert.deepEqual(test3, [10, 55, 10]);
   });
 
 });
+
